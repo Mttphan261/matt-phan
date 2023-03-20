72606de4c89d97fddceed055103d4522ec7dfa55
Matt Phan <mttphan261@gmail.com>
2023-03-19 22:32:17 -0500

    added Super Metroid

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // const h2 = document.createElement("h2");
 // h2.textContent = "This content added by Javascript";
 // document.querySelector("body").appendChild(h2); 
+function addingEventListener() {
+
+    const input = document.getElementById('snes');
+
+    function showFavGame() {
+        document.getElementById('SuperMetroid').style.display="block";
+    }
+
+    input.addEventListener('click', showFavGame);
+
+}
+
+addingEventListener()
